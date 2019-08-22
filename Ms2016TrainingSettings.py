b8669276af661c61b731512c349c197b72775c68
Eytan Kats <eytan.kats@gmail.com>
2019-08-22 23:58:00 +0300

    changed masks enumeration

--- Ms2016TrainingSettings.py
+++ Ms2016TrainingSettings.py
@@ -9,31 +9,32 @@ class Ms2016TrainingSettings(MsBaseTrainingSettings):
     def __init__(self):
 
         # 2016 dataset relative settings
-        self.data_folder = "../../Datasets/MMSEG_MICCAI2016/PreprocessedTrainingData/axial_slices_dilation_0.3_label_0.3"
+        self.data_folder = "../../Datasets/MMSEG_MICCAI2016/PreprocessedTrainingData/axial_slices_dilation_0.2_label_0.3_with_soft_staple_implemented"
         self.data_definition_file_name = "ms_info.json"
         self.data_definition_file_path = os.path.join(self.data_folder, self.data_definition_file_name)
 
         # 2016 dataset relative preprocessing settings
-        self.filters = {"mask_0": {"min_open": 10}}
-        self.training_mask_type = MaskTypes.INTERSECTION_DILATED
+        self.filters = {}  # {"mask_9": {"min_open": 1}}
+        self.training_mask_type = MaskTypes.STAPLE_DILATED
 
         # 2016 dataset relative postprocessing settings
         self.find_thr_mask_type = MaskTypes.INTERSECTION
         self.calculate_metrics_mask_types = [MaskTypes.INTERSECTION,
-                                             MaskTypes.STAPLE,
+                                             MaskTypes.LOP_STAPLE,
                                              MaskTypes.EXPERT_1,
                                              MaskTypes.EXPERT_2,
                                              MaskTypes.EXPERT_3,
                                              MaskTypes.EXPERT_4,
                                              MaskTypes.EXPERT_5,
                                              MaskTypes.EXPERT_6,
-                                             MaskTypes.EXPERT_7]
+                                             MaskTypes.EXPERT_7,
+                                             MaskTypes.STAPLE]
 
         # 2016 dataset relative model compilation settings
         self.input_shape = (216, 180, 5)
 
         # 2016 dataset relative model training settings
-        self.epochs = 3000
+        self.epochs = 5000
         self.steps_per_epoch = 16
         self.batch_size = 32
         self.val_steps = 8
