f243a67b769003820040a611b9cc872bd247365f
Eytan Kats <eytan.kats@gmail.com>
2019-08-22 23:55:36 +0300

    added fields to support conditional dilation postprocessing and sorting before inference

--- MsBaseTrainingSettings.py
+++ MsBaseTrainingSettings.py
@@ -23,28 +23,33 @@ class MsBaseTrainingSettings(abc.ABC):
 
         # Data generator settings
         self.data_random_seed = 2018
-        self.folds = 1
+        self.folds = 4
         self.train_split = 0.8
         self.test_split = 0.1  # useful only in 1-fold setting without leave out setting
         self.leave_out = True  # allows to choose for test data with unique values of 'self.leave_out_param'
         self.leave_out_param = 'patient'
-        self.leave_out_values = [0, 5, 10]
+        self.leave_out_values = [[0, 5, 10], [1, 6, 11], [2, 7, 12], [3, 8, 13], [4, 9, 14]]
         self.generator = Generator(self)
 
         # Data preprocessing settings
-        self.preload_data = True
-        self.preload_labels = True
+        self.preload_data = False
+        self.preload_labels = False
         self.crop = True
         self.crop_offset = [[6, 7], [6, 7], [0, 0]]
 
         # Data postprocessing settings
-        self.opt_thr = 0.01
-        self.find_opt_thr = True
+        self.opt_thr = 0.5
+        self.find_opt_thr = False
         self.thrs_to_check = np.arange(0.01, 1, 0.01)
+        self.small_objects = 18
+        self.dilation_iterations = 1
+        self.dilation_thr = 1.5
+        self.data_thr = 0.97
+        self.minimum_overlap = 1
         self.data_utils = DataUtils(self)
 
         # Output settings
-        self.simulation_folder = "../../Simulations/MsSegmentation/test"
+        self.simulation_folder = "../../Simulations/MsSegmentation/slc_1/5_fold_staple_implemented_mask_soft_staple_implemented_slices_0.5"
         self.train_data_file_name = "train_data.json"
         self.val_data_file_name = "val_data.json"
         self.test_data_file_name = "test_data.json"
@@ -68,6 +73,9 @@ class MsBaseTrainingSettings(abc.ABC):
         self.load_weights_name = "best_weights.h5"
         self.load_weights_path = os.path.join(self.simulation_folder, self.load_weights_name)
         self.train_model = False
+
+        # Inference settings
+        self.sort_columns = ["patient", "slice"]
 
         # Callbacks settings
         self.callbacks_container = KerasCallbacks(self)
